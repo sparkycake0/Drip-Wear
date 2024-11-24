ea7c63ce5644d213aecba9257420301e4d4c7bb2
sparkycake0 <ogirajko02@gmail.com>
2024-11-24 17:47:07 +0100

    asd

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,27 +1,36 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import drip from './crop.png'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import drip from "./crop.png";
 export default function Navbar() {
   return (
-    <main className='flex flex-row items-center justify-between border-0 h-14 gap-10 bg-white shadow-xl font-bold'>
-        <div>
-            <Image src={drip} width={90} className='absolute top-1.5 left-3'/>
-        </div>
-        <div className='flex flex-row gap-10'>
-            <Link href={"/"}>
-                <h1 className='hover:bg-neutral-300 p-2 rounded-xl transition-all'>Products</h1>
-            </Link>
-            <Link href={"/account"}>
-                <h1 className='hover:bg-neutral-300 p-2 rounded-xl transition-all'>Account</h1>
-            </Link>
-            <Link href={"/aboutus"}>
-                <h1 className='hover:bg-neutral-300 p-2 rounded-xl transition-all'>About us</h1>
-            </Link>
-        </div>
-        <div>
-
-        </div>
+    <main className="flex flex-row items-center justify-between border-0 h-14 gap-10 bg-white shadow-xl font-bold">
+      <div>
+        <Image
+          alt="asd"
+          src={drip}
+          width={90}
+          className="absolute top-1.5 left-3"
+        />
+      </div>
+      <div className="flex flex-row gap-10">
+        <Link href={"/"}>
+          <h1 className="hover:bg-neutral-300 p-2 rounded-xl transition-all">
+            Products
+          </h1>
+        </Link>
+        <Link href={"/account"}>
+          <h1 className="hover:bg-neutral-300 p-2 rounded-xl transition-all">
+            Account
+          </h1>
+        </Link>
+        <Link href={"/aboutus"}>
+          <h1 className="hover:bg-neutral-300 p-2 rounded-xl transition-all">
+            About us
+          </h1>
+        </Link>
+      </div>
+      <div></div>
     </main>
-  )
+  );
 }
